089cde80833e65db536b48785c5d293452317259
Ilix-studio <ilishjyoti17@gmail.com>
2022-12-11 19:19:06 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
-const express = require("express")
+const express = require("express");
+const connectDB  = require('./config/db');
 const app = express()
 const PORT = process.env.PORT;
 
@@ -10,3 +11,5 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('hi')
   })
+
+  connectDB();
