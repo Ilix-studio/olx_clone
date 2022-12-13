978131295c544176e56dec265c7b14d42777cd65
Ilix-studio <ilishjyoti17@gmail.com>
2022-12-14 03:09:18 +0530

    jwt and bcrypt compare password added and it is working

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,10 +1,12 @@
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
 const User = require("../models/userSchema");
 
 //desc - register User
 //route - POST /api/user
 //Public
-const registerUser = async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     res.status(400);
@@ -16,51 +18,68 @@ const registerUser = async (req, res) => {
     throw new Error("User Already Exists");
   }
 
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   const user = await User.create({
     name,
     email,
-    password,
+    password: hashedPassword,
   });
   if (user) {
-    res.status(301).json({
+    res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email,
-    }); 
-  }else {
+      token: generateToken(user._id),
+    });
+  } else {
     res.status(400);
     throw new Error("Invalid user data");
   }
-};
+});
 //desc - login User
 //route - POST /api/user
 //Public
-const loginUser = async(req, res) => {
-  const {email, password} = req.body;
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
-  if(user){
+  if (user  && (await bcrypt.compare(password, user.password))) {
     res.json({
-        _id: user.id,
-        name: user.name,
-        email: user.email,  
-    })
-  }else {
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
     res.status(400);
     throw new Error("Invalid Credentials");
   }
-};
+});
 
 //desc - login User
 //route - GET /api/user
 //privaqte
-const getUser = async(req, res) => {
-    const { _id, name, email } = await User.findById(req.user.id);
-    res.status(200).json({
-        id: _id,
-        name,
-        email,
-      });
+const getUser = asyncHandler(async (req, res) => {
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
+});
+
+
+//Generate Token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
+
+
+
 module.exports = {
   registerUser,
   loginUser,
