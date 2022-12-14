cf92429fd91eac9eaed55833b3dacc802ee19e95
Ilix-studio <ilishjyoti17@gmail.com>
2022-12-15 00:22:38 +0530

    products can be create and read from db

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,21 +1,63 @@
-const getProduct = () => {
+const UserProduct = require("../models/ProductSchema");
+const User = require("../models/userSchema");
+const multer = require("multer");
+const expressAsyncHandler = require("express-async-handler");
 
-}
-const createProduct = () => {
+const getProduct = async (req, res) => {
+  const userProducts = await UserProduct.find();
+  res.status(200).json(userProducts);
+};
+//image storage path
+const imgconfig = {};
 
-}
-const updateProduct = () => {
+//image Filter
+const isImage = {};
 
-}
-const deleteProduct = () => {
+const upload = multer({
+  storage: imgconfig,
+  fileFilter: isImage,
+});
 
-}
+//desc -  create User Product
+//route - POST /api/user
+//Private
+const createProduct = expressAsyncHandler(async (req, res) => {
+  const { productName, price, image, phno } = req.body;
+  if (!productName || !price || !image || !phno) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+  const user = await User.findById(req.user.id)
+  const userProduct = await UserProduct.create({
+    user,
+    productName,
+    price,
+    image,
+    phno,
+  });
+  if (userProduct) {
+    res.status(201).json({
+        
+            
+            user:req.body.id,
+            productName:userProduct.productName,
+            price: userProduct.price,
+            image: userProduct.image,
+            phno: userProduct.phno
+        
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid product data");
+  }
+});
 
+const updateProduct = async (req, res) => {};
+const deleteProduct = async (req, res) => {};
 
 module.exports = {
-    getProduct,
-    createProduct,
-    updateProduct,
-    deleteProduct
-
-}
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,8 +5,9 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controller/productController");
+const {protect} = require("../middleware/authMiddleware")
 const router = express.Router();
 
-router.route("/").get(getProduct).post(createProduct);
-router.route("/:id").put(updateProduct).delete(deleteProduct);
+router.route("/").get(protect,getProduct).post(protect,createProduct);
+router.route("/:id").put(protect,updateProduct).delete(protect,deleteProduct);
 module.exports = router;
