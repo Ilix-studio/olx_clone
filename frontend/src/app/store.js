805a58ca80f581d4f1e67781a8ccb2359a89df0f
Ilix-studio <ilishjyoti17@gmail.com>
2022-12-15 23:07:52 +0530

    reducer Slice add to store, update to UI left

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from "../features/auth/authSlice"
-
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import productReducer from "../features/userProducts/productSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    products: productReducer,
   },
 });
--- frontend/src/features/userProducts/productSlice.js
+++ frontend/src/features/userProducts/productSlice.js
@@ -49,29 +49,72 @@ export const deleteProduct = createAsyncThunk(
   "goals/delete",
   async (id, thunkAPI) => {
     try {
-        const token = thunkAPI.getState().auth.user.token
-        return await productService.deleteProduct(id, token)
+      const token = thunkAPI.getState().auth.user.token;
+      return await productService.deleteProduct(id, token);
     } catch (error) {
-        const message =
+      const message =
         (error.response &&
           error.response.data &&
           error.response.data.message) ||
         error.message ||
-        error.toString()
-      return thunkAPI.rejectWithValue(message)
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
     }
   }
 );
 
 export const productSlice = createSlice({
-    name: 'product',
-    initialState,
-    reducers:{
+  name: "product",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.products.push(action.payload);
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
 
-    },
-    extraReducers: (builder) => {
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.products = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.products = action.payload;
+      })
 
-    }
+      .addCase(deleteProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.products = state.products.filter(
+          (product) => product._id !== action.payload.id
+        );
+      })
+      .addCase(deleteProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
-
-export default productSlice.reducer
+export const { reset } = productSlice.actions;
+export default productSlice.reducer;
